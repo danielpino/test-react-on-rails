351a047f1c5f286c079451679a8a8f4e6fc56045
Daniel Pino <danielpino@Daniels-MacBook-Pro.local>
2018-02-08 22:21:38 -0500

    committing

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+
+  Rails.application.routes.draw do
+    namespace :api do
+      resources :artists do
+        resources :songs
+      end
+    end
+  end
+  
+end
